ccadc8c79fe70debca018655e2fcbc7fc233c17a
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2025-11-11 22:57:37 +0200

    11Nov
    partners of memphis

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,25 @@
 // import Image from "next/image";
 
+import StayInformedSection from "@/components/forms/stayInformed";
+import Partners from "@/components/partners/page";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"
-      style={{width:"100%", minHeight:'70svh'
-      }}
-    >
-      <h1>home page of website</h1>
-    </div>
+    <section>
+      {/* heroblock with video playing in background, overlayed with dark transparent overlay, logo and "INVESTING WHERE IT'S NEEDED MOST" */}
+
+      {/* WHO WE ARE - 2 column block with words left and video right; learn more button leading to About Us Page */}
+
+      {/* section with 3 link blocks (What we do; How we help; join BCOM) */}
+
+      {/* block with buttons for news, events */}
+
+      {/* Section for member directory with buttons - join the directory and search the directory */}
+
+      <Partners/>
+
+      <StayInformedSection/>
+
+    </section>
   );
 }
---
+++ components/partners/page.tsx
@@ -0,0 +1,197 @@
+'use client'
+import { useState } from "react"
+import { clsx } from "clsx"
+import style from '@/styles/partners.module.css'
+import Image from "next/image"
+
+const publicPartners = [
+    {
+        logo: "public-partner-100blackmen",
+        alt: "100 Black Men of Memphis, Inc"
+    },{
+        logo: "public-partner-city-of-memphis",
+        alt: "City of Memphis"
+    },{
+        logo: "public-partner-community-lift",
+        alt: "Community Lift"
+    },{
+        logo: "public-partner-greater-memphis-chamber",
+        alt: "Greater of Memphis Chamber"
+    },{
+        logo: "public-partner-lemoyne-owen",
+        alt: "LeMoyne Owen College"
+    },{
+        logo: "public-partner-mamca",
+        alt: "Memphis Area Minority Contractors Association"
+    },{
+        logo: "public-partner-mmbc-continuum",
+        alt: "Mid-South Minority Business Council Continuum"
+    },{
+        logo: "public-partner-nareb-realtist",
+        alt: "National Association of Real Estate Brokers"
+    },{
+        logo: "public-partner-nawbo",
+        alt: "NAWBO Memphis"
+    },{
+        logo: "city_of_memphis_contract_compliance",
+        alt: "City Of Memphis Contract Compliance"
+    },{
+        logo: "public-partner-river-city-capital-investment",
+        alt: "River City Capital Investment"
+    },{
+        logo: "public-partner-sba",
+        alt: "Small Business Administration"
+    },{
+        logo: "public-partner-sbdc",
+        alt: "Tennessee Small Business Development Center"
+    },{
+        logo: "public-partner-tsmsdc",
+        alt: "TSMSDC"
+    },{
+        logo: "public-partner-usbc",
+        alt: "united States Black Chamber Incorporated"
+    },{
+        logo: "public-partner-epicenter",
+        alt: "Epicenter"
+    },{
+        logo: "public-partner-bldg-memphis",
+        alt: "Build, Live, Develop, Grow"
+    }
+]
+
+const strategicPartners = [
+    {
+        logo: "stratigic-partner-small-business-association",
+        alt: "Small Business Association"
+    },{
+        logo: "city_of_memphis_contract_compliance",
+        alt: "City Of Memphis Contract Compliance"
+    }
+]
+
+const corporatePartners = [
+    {
+        logo: "corporate-partner-tva",
+        alt: "Tennessee Valey Authority"
+    },{
+        logo: "corporate_partner-autozone",
+        alt: "AutoZone"
+    },{
+        logo: "corporate_partner-fedex",
+        alt: "FedEx"
+    },{
+        logo: "corporate_partner-nike",
+        alt: "Nike"
+    },{
+        logo: "corporate_partner-paragon",
+        alt: "Paragon Bank"
+    },{
+        logo: "corporate-partners-pinnacle",
+        alt: "Pinnale Financial Partners"
+    },{
+        logo: "corporate-partner-seal-of-shelby-county",
+        alt: "Seal of Shelby County"
+    }
+
+]
+
+export default function Partners(){
+
+    const [ isPublicPartners, setPublicPartner ] = useState(true);
+    const [ isStratigicPartners , setStratigicPartners ] = useState(false);
+    const [ isCorporatePartners, setCorporatePartners ] = useState(false)
+
+    return(
+        <section
+            className={style.partnersSection}
+        >
+            <h2
+                className={style.partnersHeading}
+            >
+                OUR PARTNERS
+            </h2>
+
+            <div
+                className={style.partnersButtonRow}
+            >
+                <button
+                    className={clsx(style.partnersButton, isPublicPartners && style.partnersActiveButton)}
+                    onClick={() => {
+                        setPublicPartner(true)
+                        setCorporatePartners(false)
+                        setStratigicPartners(false)
+                    }}
+                >
+                    Public Partners
+                </button>
+
+                <button
+                    className={clsx(style.partnersButton, isStratigicPartners && style.partnersActiveButton)}
+                    onClick={() => {
+                        setPublicPartner(false)
+                        setCorporatePartners(false)
+                        setStratigicPartners(true)
+                    }}
+                >
+                    Stratigic Partners
+                </button>
+
+                <button
+                    className={clsx(style.partnersButton, isCorporatePartners && style.partnersActiveButton)}
+                    onClick={() => {
+                        setPublicPartner(false)
+                        setCorporatePartners(true)
+                        setStratigicPartners(false)
+                    }}
+                >
+                    Corporate Partners
+                </button>
+            </div>
+
+            <div
+                className={clsx(style.hide, isPublicPartners && style.partnersLogoBlock)}
+            >
+                {publicPartners.map(({logo, alt}, z) => (
+                    <Image
+                        key={z}
+                        src={"/partners/" + logo + ".webp"}
+                        alt={alt}
+                        width={300}
+                        height={300}
+                        className={style.partnersLogo}
+                    />
+                ))}
+            </div>
+
+            <div
+                className={clsx(style.hide, isStratigicPartners && style.partnersLogoBlock)}
+            >
+                {strategicPartners.map(({logo, alt}, z) => (
+                    <Image
+                        key={z}
+                        src={"/partners/" + logo + ".webp"}
+                        alt={alt}
+                        width={300}
+                        height={300}
+                        className={style.partnersLogo}
+                    />
+                ))}
+            </div>
+
+            <div
+                className={clsx(style.hide, isCorporatePartners && style.partnersLogoBlock)}
+            >
+                {corporatePartners.map(({logo, alt}, z) => (
+                    <Image
+                        key={z}
+                        src={"/partners/" + logo + ".webp"}
+                        alt={alt}
+                        width={300}
+                        height={300}
+                        className={style.partnersLogo}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
