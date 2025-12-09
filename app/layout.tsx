8ab1bac8fd9a625ebd8f67ee18e3152f2661f8e0
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2025-12-09 16:49:11 +0200

    9Dec2
    Images for leveledUp banner
    Google Tag Manager code

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* ⭐ Google Tag Manager – HEAD Script */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NMSBD5BR');
+            `,
+          }}
+        />
+
         {/* --- JSON-LD Organization Schema --- */}
         <Script
           id="organization-schema"
@@ -90,9 +105,17 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        {/* ⭐ Google Tag Manager – BODY noscript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMSBD5BR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header/>
         {children}
         <Footer/>
