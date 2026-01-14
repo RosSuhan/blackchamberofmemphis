88eb3752ad3861e45e23cbffe0e5a9927ac16e71
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-01-14 23:52:50 +0200

    14Jan3
    fixing type property bugs

--- lib/members/membersListType.ts
+++ lib/members/membersListType.ts
@@ -1,16 +1,16 @@
 export interface MembersListType {
     memberStatus: boolean,
     id: string;
-    businessName?: string;
-    bannerImage?: string;
-    profileLogo?: string;
-    profileDescription?: string;
-    profileCategory?: string[];
-    profileSubCategory?: string[];
-    categoryTag?: string[],
-    profileBusinessDescription?: string;
+    businessName: string;
+    bannerImage: string;
+    profileLogo: string;
+    profileDescription: string;
+    profileCategory: string[];
+    profileSubCategory: string[];
+    categoryTag: string[],
+    profileBusinessDescription: string;
     gallery?: { image: string; alt: string }[];
-    businessHours?: {
+    businessHours: {
         monHours: string;
         tueHours: string;
         wedHours: string;
@@ -19,17 +19,17 @@ export interface MembersListType {
         satHours: string;
         sunHours: string;
     };
-    addressLink?: string;
-    address?: string;
-    email?: string;
-    website?: string;
-    telNumber?: string;
-    facebook?: string;
-    instagram?: string;
-    youtube?: string;
-    linkedIn?: string;
-    linktree?: string;
-    twitter?: string;
-    tiktok?: string;
+    addressLink: string;
+    address: string;
+    email: string;
+    website: string;
+    telNumber: string;
+    facebook: string;
+    instagram: string;
+    youtube: string;
+    linkedIn: string;
+    linktree: string;
+    twitter: string;
+    tiktok: string;
     pinterest?: string;
 }
