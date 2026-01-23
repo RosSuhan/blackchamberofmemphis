a437efbb692348e79b2473906977a0c4db77243a
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-01-23 17:30:57 +0200

    23Jan01
    Leveled Up Money

--- lib/eventsList/eventIndex.ts
+++ lib/eventsList/eventIndex.ts
@@ -2,10 +2,16 @@ import { leveledUp } from "./eventsFolder/leveledUp";
 import { bcomMixer } from "./eventsFolder/bcomMixer";
 import { janBcomNewMember } from './eventsFolder/janBcomNewMember';
 import { febBcomNewMember } from './eventsFolder/febBcomNewMember'
+import { leveledUp2 } from "./eventsFolder/leveledUp2";
+import { leveledUp3 } from "./eventsFolder/leveledUp3";
+import { leveledUp4 } from "./eventsFolder/leveledUp4";
 
 export const eventIndex = [
     leveledUp,
     bcomMixer,
     janBcomNewMember,
-    febBcomNewMember
+    febBcomNewMember,
+    leveledUp2,
+    leveledUp3,
+    leveledUp4,
 ].sort((a, b) => new Date(a.sortDate).getTime() - new Date(b.sortDate).getTime());
