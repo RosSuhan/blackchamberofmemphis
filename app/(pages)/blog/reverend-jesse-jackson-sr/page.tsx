c0a640b877aa2f324d37eee97dc3a9365e08c8b2
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-02-23 17:28:17 +0200

    Jesse Jackson
    Memorial for Jesse Jackson

---
+++ app/(pages)/blog/reverend-jesse-jackson-sr/page.tsx
@@ -0,0 +1,53 @@
+import PageTitleSection from '@/components/pageTitleSection/page'
+import style from './reverend.module.css'
+
+export default function ReverendJesseJackson(){
+    return(
+        <main
+            className={style.reverendJessePage}
+        >
+            <PageTitleSection
+                pageTitle='Honoring a Tireless Advocate for Equality'
+            />
+
+            <section
+                className={style.reverendJesseTextSection}
+            >
+
+                <article
+                    className={style.reverendJesseArticle}
+                >
+                    <p>
+                        The Black Chamber of Memphis respectfully acknowledges the passing of Reverend Jesse Jackson Sr., whose significant contributions impacted communities, inspired generations, and advanced the moral conscience of the nation. His steadfast commitment to civil rights, voting rights, economic justice, and human dignity fundamentally shaped American democracy. Reverend Jackson consistently advocated for those whose voices often went unheard, promoted equality, and worked toward bridging divisions.
+                    </p>
+                    <p>
+                        Through his leadership and advocacy, Reverend Jackson demonstrated that achieving justice requires perseverance, that dignity is inherent for all individuals, and that unity is more powerful than discord. While we mourn his loss, we also recognize the enduring legacy of his work in expanded opportunities, protected rights, and inspired leaders who carry forward his vision.
+                    </p>
+                    <p>
+                        We honor Reverend Jackson’s memory by upholding the principles he championed: justice, equality, compassion, and hope.
+                    </p>
+                    <p>
+                        With heartfelt sympathy and deepest condolences.
+                    </p>
+
+                    <p><strong>The Black Chamber of Memphis & Staff</strong></p>
+                    <p><strong>Mark Yates, Board Chair/President & CEO</strong></p>
+                </article>
+                <div
+                    className={style.reverendJesseVideoBlock}
+                >
+                    <iframe 
+                        src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1247020440712843%2F&show_text=false&width=267&t=0" 
+                        width="267" 
+                        height="476" 
+                        style={{border:"none", overflow:"hidden"}} 
+                        scrolling="no" 
+                        frameBorder="0" 
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" 
+                        className={style.reverendJesseVideo}
+                        ></iframe>
+                </div>
+            </section>
+        </main>
+    )
+}
