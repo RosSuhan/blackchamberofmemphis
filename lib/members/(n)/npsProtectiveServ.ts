37963799a28aa6fd60c2be287a96070543e31338
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-02-25 00:40:33 +0200

    25Feb
    updating monyeality and NPS Protective services

--- lib/members/(n)/npsProtectiveServ.ts
+++ lib/members/(n)/npsProtectiveServ.ts
@@ -6,7 +6,7 @@ export const npsProtectiveService : MembersListType = {
     businessName : "NPS Protective Service",
     bannerImage : "",
     profileLogo : "nps-protective-service",
-    profileDescription : "NPS Protective Service is a Millington, Tennessee–based security firm offering armed and unarmed guarding, mobile patrols, security training, and customized protection services for businesses, properties, and organizations in the region. ",
+    profileDescription : "NPS Protective Service is a Millington, Tennessee–based security firm offering armed and unarmed guarding,  security training, and customized protection services for businesses, properties, and organizations in the region.",
     profileCategory : [
         "security-protection-services"
     ],
@@ -19,9 +19,19 @@ export const npsProtectiveService : MembersListType = {
     categoryTag: [
         "mobile patrols", 
         "security training", 
-        "customized protection services for businesses"
+        "customized protection services for businesses",
+        "active shooter survival training",
+        "security guard services",
+        "armed security",
+        "unarmed security",
+        "first aid, cpr and aed training"
     ],
-    profileBusinessDescription : ``,
+    profileBusinessDescription : `
+        <p>NPS Protective Service provides comprehensive security solutions out of its headquarters at 7664 US Highway 51 N, Suite 2, Millington, TN.  With a foundation of more than 20 years of combined military, law-enforcement, and professional security experience, the firm brings seasoned expertise to both armed and unarmed protection services.</p>
+        <p>The company’s offerings are designed to meet a wide variety of security needs — from on-site guarding and parking-lot patrols to warehouse security, risk assessments, and specialized training programs (including de-escalation and active-shooter preparation). NPS also provides flexible mobile patrol services for clients with multiple properties or shifting security needs.</p>
+        <p>With a client-centered approach, NPS emphasizes professionalism, responsiveness, and a strong commitment to safeguarding people, property, and business assets. The firm’s staff is carefully selected, licensed, and trained to deliver high-quality security with integrity and discretion.</p>
+        <p>Whether you run a warehouse, commercial property, retail location, or manage a larger estate — and whether you need static guarding, patrols, alarm response, or security training — NPS Protective Service positions itself as a reliable, versatile security partner capable of customizing its services to meet each client’s unique demands.</p>
+        <p>Contact can be made via phone at (901) 872-6771 or through their website for service quotes and more information.</p>`,
     gallery : [],
     businessHours : [
         { day : "Monday", hours : "9 am - 4:30 pm",},
@@ -34,7 +44,7 @@ export const npsProtectiveService : MembersListType = {
     ],
     addressLink : "https://share.google/tRZ5OcI0bkZbeu7HV",
     address : "7664 US-51 suite 2, Millington, TN 38053",
-    email : "",
+    email : "president@npsprotectservice.com",
     website : "https://www.npsprotectservice.com/",
     telNumber : "901-872-6771",
     facebook : "https://www.facebook.com/profile.php?id=61579349197709",
@@ -43,6 +53,6 @@ export const npsProtectiveService : MembersListType = {
     linkedIn : "",
     linktree : "",
     twitter: "https://x.com/eric_norto39875",
-    tiktok : "",
+    tiktok : "https://www.tiktok.com/@npsprotectiveservice",
     pinterest : ""
 }
