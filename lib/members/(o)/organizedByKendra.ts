0635970e348ddbcfa9641db9c89e25e17973a689
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-03-04 21:52:03 +0200

    4March
    uploading of Organized by Kendra

---
+++ lib/members/(o)/organizedByKendra.ts
@@ -0,0 +1,51 @@
+import { MembersListType } from './../membersListType'
+
+export const organizedByKendra : MembersListType = {
+    memberStatus: false,
+    id : "organized-by-kendra",
+    businessName : "Organized by Kendra",
+    bannerImage : "",
+    profileLogo : "organized-by-kendra",
+    profileDescription : "Organized by Kendra, LLC is a full-service interior organization + styling company based in Southaven, MS, near Memphis, TN.  We provide residential and commercial services to ambitious professionals, entrepreneurs, and creatives in the Memphis metro area and beyond.",
+    profileCategory : [
+        "professional-legal-financial-services",
+    ],
+    profileSubCategory : [
+        'professional-organizing-services',
+    ],
+    categoryTag: [
+        'Home Organization & Decluttering',
+        'Residential & Commercial Space Optimization',
+        'Virtual Organization & Productivity Services',
+        'Home Staging & Move-In/Move-Out Support',
+        'Concierge & Lifestyle Support Services',
+    ],
+    profileBusinessDescription : `
+        <p>Organized by Kendra is a professional home-organizing and strategic space planning company founded by Kendra Littlejohn and based in Southaven, Mississippi, serving the Memphis metropolitan area and beyond. The company helps busy professionals, entrepreneurs, and creatives transform cluttered, overwhelming spaces into organized environments that support productivity, clarity, and ease in everyday life.</p>
+        <p>With more than 18 years of experience in corporate finance prior to launching the business, Kendra brings a unique blend of analytical thinking and practical design to her work. Organized by Kendra specializes in whole-home organization, move preparation and unpacking, closet and kitchen systems, and personalized space solutions that balance functionality with aesthetics. Each project is approached with both strategy and care, ensuring that every space not only looks beautiful but also operates efficiently for the client’s lifestyle.</p>
+        <p>The company’s work is guided by Kendra’s signature PACE Framework: Purge, Arrange, Create Zones, and Establish Systems. This method allows clients to move beyond temporary tidying and instead build sustainable systems that make it easier to maintain order long after the project is complete.</p>
+        <p>Organized by Kendra has been featured in national publications including ESSENCE, Real Simple, and Southern Living, and is known for delivering high-touch, professional service. At its core, the company’s mission is to help clients eliminate clutter, restore harmony in their homes, and create spaces that allow them to live and work with greater ease.<p>`,
+    gallery : [],
+    businessHours : [
+        { day : "Monday", hours : 'Closed'},
+        { day : "Tuesday", hours : '10:30am - 3:30pm'},
+        { day : "Wednesday", hours : '10:30am - 3:30pm'},
+        { day : "Thursday", hours : '10:30am - 3:30pm'},
+        { day : "Friday", hours : '10:30am - 3:30pm'},
+        { day : "Saturday", hours : '10:30am - 3:30pm'},
+        { day : "Sunday", hours : '10am - 1pm'},
+    ],
+    addressLink : "https://share.google/Gshpr5SpDgWpN4MRH",
+    address : "Memphis",
+    email : "hi@organizedbykendra.com",
+    website : "https://www.organizedbykendra.com",
+    telNumber : "9012180712",
+    facebook : "https://www.facebook.com/organizedbykendra",
+    instagram : "https://www.instagram.com/organizedbykendra",
+    youtube : "",
+    linkedIn : "",
+    linktree : "https://beacons.ai/oklinks",
+    twitter: "",
+    tiktok : "",
+    pinterest : ""
+}
--- lib/members/membersOlist.ts
+++ lib/members/membersOlist.ts
@@ -1,5 +1,7 @@
 import { otekaTechnologies } from "./(o)/otekaTechnologies";
+import { organizedByKendra } from "./(o)/organizedByKendra";
 
 export const membersOlist = [
     otekaTechnologies,
+    organizedByKendra,
 ]
