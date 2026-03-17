7039f87cfa0ad53cdda032b17716535e482816bd
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-03-17 16:39:45 +0200

    chalmers pool

---
+++ lib/members/(c)/chalmersPoolePC.ts
@@ -0,0 +1,38 @@
+import { MembersListType } from './../membersListType'
+
+export const template : MembersListType = {
+    memberStatus: true,
+    id : "chalmers-poole-pc",
+    businessName : "Chalmers & Poole PC",
+    bannerImage : "",
+    profileLogo : "chalmerspoole",
+    profileDescription : "Chalmers Poole is a Memphis-based accounting and financial planning firm providing comprehensive tax preparation, financial strategy, and wealth planning services. The firm serves individuals, business owners, and executives with personalized solutions designed to minimize taxes, preserve wealth, and support long-term financial goals.",
+    profileCategory : [
+        "professional-legal-financial-services",
+    ],
+    profileSubCategory : [
+        "accountants",
+        "financial-planner",
+        "tax-services",
+        "business-consultants",
+    ],
+    categoryTag: [
+        'business consulting',
+    ],
+    profileBusinessDescription : ``,
+    gallery : [],
+    businessHours : [],
+    addressLink : "",
+    address : "",
+    email : "achalmers@chalmers-poole.com",
+    website : "https://www.chalmers-poole.com/",
+    telNumber : "(901) 763-3335",
+    facebook : "",
+    instagram : "",
+    youtube : "",
+    linkedIn : "",
+    linktree : "",
+    twitter: "",
+    tiktok : "",
+    pinterest : ""
+}
