bcdf76f954905b8c93d18606028ae048053d5c6a
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-06-16 19:22:12 +0200

    fixing update

--- app/(pages)/sandbox/page.tsx
+++ app/(pages)/sandbox/page.tsx
@@ -25,6 +25,7 @@ export default async function Sandbox(){
                 eventDate = {''} 
                 eventTime = {''} 
                 eventAddress = {''}
+                stateButtons={[]}
             />
         </main>
     )
