8009f7ffc6e99b237ca41f7f6c19409109f24c22
Rosalyn van Zyl <125730414+RosSuhan@users.noreply.github.com>
2026-07-14 18:37:14 +0200

    Symposium address and date fix

--- components/EventsBlock/eventsPageSection.tsx
+++ components/EventsBlock/eventsPageSection.tsx
@@ -38,10 +38,10 @@ export default function EventsPageSection(){
                     featuredImageWidth = {2048}
                     featuredImageHeight = {1363}
                     featuredEventName = {'Building Wealth In Our Community Symposium'}
-                    featuredEventDateTime = {'August 28, 2026'}
+                    featuredEventDateTime = {'October 2, 2026'}
                     featuredEventPrice = {"$50 (Member); $60 (Non-member)"}
                     featuredEventAddressLink = {"/events/symposium"}
-                    featuredEventAddress = {"Epicenter, 150 Peabody Pl, Memphis, TN 38103, United States"}
+                    featuredEventAddress = {"Reign Event Venue, 3243 Players Club Cir, Memphis, TN 38125"}
                     featuredEventDescription = {"The Black Chamber of Memphis Symposium is a flagship event focused on advancing entrepreneurship, economic mobility, and wealth-building within the community. Bringing together business owners, leaders, and professionals, the event offers a dynamic mix of networking, expert-led discussions, and practical insights designed to help entrepreneurs start, grow, and scale sustainable businesses."}
                     featuredEventActionLink = {`/events/symposium`}
                     featuredEventActionBtnText = {'Read More'}
